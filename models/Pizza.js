ba3ece0b3c5d7ed5beabab56f668f0387e1f5b15
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-26 19:14:27 -0500

    create pizza model and connect mongo database

---
+++ models/Pizza.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
+
